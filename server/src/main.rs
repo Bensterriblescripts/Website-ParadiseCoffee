c753ddc50099764e0e0f27e8b7ba6442937dcb01
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-08-28 22:27:12 +1200

    work

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,39 +1,20 @@
 use std::net::SocketAddr;
-use serde::Deserialize;
 
-use axum::Json;
-use axum::response::IntoResponse;
 use axum::response::Html;
-use axum::routing::get;
 use axum::Router;
 
 #[tokio::main]
 async fn main() {
+    let routes_hello = Router::new().route(
+        "/", get(hello)
+    );
 
-    // Endpoint
-    let routes = Router::new().route("/search", get(search));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("--> Listening on {addr}\n");
+    println!("LISTENING on {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes.into_make_service())
-        .await
-        .unwrap();
+        .serve(routes_hello.into_make_service())
 }
 
-// API
-#[derive(Debug, Deserialize)]
-struct SearchParams {
-    dn: Option<String>,
-    filter: Option<String>,
-    attributes: Option<Vec<String>>,
-}
-async fn search (Json(params): Json<SearchParams>) -> impl IntoResponse {
-
-    println!("->> {:<12} - Search - {params:?}", "Handler");
-
-    let ldap_dn = params.dn.as_deref().unwrap_or("cn=admin,dc=paradisecoffee,dc=cafe");
-    let ldap_filter = params.filter.as_deref().unwrap_or("sn=*");
-    let ldap_attr: Vec<String> = params.attributes.unwrap_or(Vec::new());
-
-    Html(format!("DN: {ldap_dn}\n\nFilter: {ldap_filter}\n\nAttributes: {ldap_attr:?}"))
+async fn hello () {
+    Html("Hello")
 }
