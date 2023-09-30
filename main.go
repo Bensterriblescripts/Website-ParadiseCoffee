db35ed0b2d501cad60871634c212548b68f7f607
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-09-30 19:12:54 +1300

    fixed logo, removed db connection, added logo on navbar

--- main.go
+++ main.go
@@ -1,36 +1,12 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-
-	_ "github.com/lib/pq"
-)
-
-// DB Settings
-const (
-	host     = "192.168.0.107"
-	port     = 5432
-	user     = "postgres"
-	password = "Worldwarcraft3!@"
-	dbname   = "postgres"
 )
 
 func main() {
-
-	// DB
-	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlconn)
-	if err != nil {
-		panic(err)
-	}
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Connected to the database...")
 
 	// HTTP Server
 	fileServer := http.FileServer(http.Dir("./html"))
